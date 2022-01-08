fdcfabbebb7a3346c98fc968215c11d6c4e9cb4b
Egor_Ostroumov <araimo@yandex.ru>
2022-01-08 20:49:20 +0300

    xcl_to_wrd.py добавлена нумерация для строк

--- xcl_to_wrd.py
+++ xcl_to_wrd.py
@@ -3,7 +3,7 @@ from docx import Document
 
 
 def get_data(file_name, min_row, max_row):
-   """
+    """
    функция принимает имя файла, номера строк которые нужно обработать,
    и возвращает список
    :param file_name: передается имя файла (с расширением) для прочтения
@@ -11,45 +11,60 @@ def get_data(file_name, min_row, max_row):
    :param max_row: строка на которой чтение файла заканчивается
    :return:
    """
-   data_list = []
-   sheets = op.load_workbook(filename=file_name).active
-   for rows in sheets.iter_rows(min_row=min_row, max_row=max_row, min_col=2, max_col=5):
-       x = []
-       for cell in rows:
-           x.append(cell.value)
-       data_list.append(x)
-   return data_list
+    data_list = []
+    sheets = op.load_workbook(filename=file_name).active
+    for rows in sheets.iter_rows(min_row=min_row, max_row=max_row, min_col=2, max_col=5):
+        x = []
+        for cells in rows:
+            x.append(cells.value)
+        data_list.append(x)
+    return data_list
 
 
 # задаем имя файлов в качвычках и нужные нам строки min_row и max_row
-source_list = get_data('source.xlsx', 5, 25)
+source_list = get_data('all.xlsx', 5, 25)
 document = Document()
-table_2 = document.add_table(rows=1, cols=3)
+n = 0
 
 for pc_name, hdd_number, hdd_serial, pc_number in source_list:
-   row_cells_2 = table_2.add_row().cells
-   row_cells_2[0].merge(row_cells_2[-1])
-   row_cells_2[0].text = f'АРМ с именем «{str(pc_name)}» в составе:'
-   row_cells_2 = table_2.add_row().cells
-   row_cells_2[0].text = str('Системный блок')
-   row_cells_2[1].text = str('-')
-   if pc_number is None:
-      row_cells_2[2].text = str(f'-')
-   else:
-      row_cells_2[2].text = str(f'{pc_number}')
-   row_cells_2 = table_2.add_row().cells
-   if hdd_number is None:
-      row_cells_2[0].text = str('-')
-   else:
-      row_cells_2[0].text = str(f'ЖМД № {hdd_number}')
-   row_cells_2[1].text = str('-')
-   if hdd_serial is None:
-      row_cells_2[2].text = str('-')
-   else:
-      row_cells_2[2].text = str(hdd_serial)
-   row_cells_2 = table_2.add_row().cells
-   row_cells_2[0].merge(row_cells_2[-1])
-   row_cells_2[0].text = f'Периферийное оборудование: монитор, клавиатура, манипулятор мышь, принтер,акустические колонки, web-камера '
-
+    n += 1
+    table = document.add_table(rows=4, cols=4)
+    cell = table.cell(0, 0)
+    cell.text = f'{n}.'
+    cell = table.cell(0, 1)
+    cell.text = f'АРМ с именем «{str(pc_name)}» в составе:'
+    cell = table.cell(1, 1)
+    cell.text = str('Системный блок')
+    cell = table.cell(1, 2)
+    cell.text = str('-')
+    cell = table.cell(1, 3)
+    if pc_number is None:
+        cell.text = str(f'-')
+    else:
+        cell.text = str(f'{pc_number}')
+    cell = table.cell(2, 1)
+    if hdd_number is None:
+        cell.text = str('-')
+    else:
+        cell.text = str(f'ЖМД № {hdd_number}')
+    cell = table.cell(2, 2)
+    cell.text = str('-')
+    cell = table.cell(2, 3)
+    if hdd_serial is None:
+        cell.text = str('-')
+    else:
+        cell.text = str(hdd_serial)
+    cell = table.cell(3, 1)
+    cell.text = f'Периферийное оборудование: монитор, клавиатура, манипулятор мышь,' \
+                f' принтер,акустические колонки, web-камера '
+    a = table.cell(0, 1)
+    b = table.cell(0, 3)
+    a.merge(b)
+    a = table.cell(3, 1)
+    b = table.cell(3, 3)
+    a.merge(b)
+    a = table.cell(0, 0)
+    b = table.cell(3, 0)
+    table = a.merge(b)
 
 document.save('check.docx')
